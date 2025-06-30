40c7c87a4d83f341b49c2976e3eca57902c3142c
Gwunleong Lee <108258271+gwnlng@users.noreply.github.com>
2025-06-30 20:44:57 +0800

    Initial main branch commit

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # snyk-cli-log-eval
-Snyk Open Source CLI debug log evaluation tool
+
+This is a Snyk CLI debug log evaluation tool that accepts standard input and outputs a result summary of the scan metadata analysis run in a JSON object output. The scan metadata analysis is captured in the standard error corresponding to line with prefix `analytics.report:2 - [0] Data: `.
+
+## Prerequisite
+This tool is only compatible with Snyk Open Source product i.e. specifically `snyk test` log analysis.
+
+- Snyk CLI usage authenticated
+- Snyk test CLI command executed with `--all-projects` and `--debug` options
+
+## Example Commands
+
+### Single command piping standard error to eval
+```
+snyk test --all-projects --debug 2> | python3 </path/to/eval.py>
+```
+
+### Capturing redirected standard output, error to a logfile for full analysis
+```
+snyk test --all-projects --debug > debug.log 2>&1
+cat "debug.log" | python3 <path/to/eval.py>
+```
+
+## Output
+The scan metadata result summary is output in JSON format.
+
+## Output Format
+
+| Property          | Description                          | Sample Values                                              |
+|-------------------|--------------------------------------|------------------------------------------------------------|
+| scanned_manifests | List of scanned manifests file paths | ["/path/to/scanned_manifest_file"]                         |
+| skipped_manifests | List of skipped manifests file paths | ["/path/to/skipped_manifest_file"]                         |
+| error_indicator   | Flag indicator of error on scan      | true or false                                              |
+| error_message     | Generic error message                | "X/Y manifests encountered error at dependency resolution" |
+| status            | Status of scan                       | "success" or "failure"                                     |
+| duration_sec      | Duration of scan in seconds          | 11.077                                                     |
+
+### Example Output
+```
+{
+    "scanned_manifests":[
+        "/Users/REDACTED/VSCodeProjects/gs-multi-module/complete/pom.xml",
+        "/Users/REDACTED/VSCodeProjects/gs-multi-module/complete/application/pom.xml",
+        "/Users/REDACTED/VSCodeProjects/gs-multi-module/complete/library/build.gradle",
+        "/Users/REDACTED/VSCodeProjects/gs-multi-module/complete/library/pom.xml"
+    ],
+    "skipped_manifests":[
+        "/Users/REDACTED/VSCodeProjects/gs-multi-module/complete/application/build.gradle"
+    ],
+    "error_indicator":true,
+    "error_message":"1/5 manifests encountered error at dependency resolution",
+    "status":"failure",
+    "duration_sec":11.077
+}
+```
+
+## Note
+
+Snyk CLI version `1.1297.3` debug analytics returns an invalid metadata JSON object.
+This is not evident in earlier versions of Snyk CLI and will be resolved in subsequent Snyk CLI releases.
---
+++ eval.py
@@ -0,0 +1,189 @@
+import sys
+import json
+
+
+def get_trailing_string_find(line: str, substring: str) -> str:
+    """
+    Returns trailing string of line after first occurrence of the substring
+    Returns empty string if not found.
+    :param line: Line
+    :param substring: Substring
+    :return: trailing string if found, empty string otherwise
+    """
+    start_index = line.find(substring)
+    if start_index != -1:  # Check if the substring was found
+        # Slice from the character right after the substring
+        return line[start_index + len(substring):]
+    else:
+        return ""  # Substring not found
+
+
+def extract_metadata_by_prefix(prefix: str) -> dict:
+    """
+    Reads lines from standard input and extract first occurring line that contains a specified prefix.
+    It then slices off prefix, extracting trailing string as a metadata JSON
+    :param prefix: prefix string
+    :return: json
+    """
+    duration_ms_prefix = '"durationMs": '
+    duration_ms = 0
+    invalid_metadata = False
+    metadata_json = {}
+    analytics_metadata = ""
+    # Read input line by line from stdin
+    for line in sys.stdin:
+        # .strip() removes leading/trailing whitespace, including newline characters,
+        # ensuring a clean match against the prefix.
+        if prefix in line.strip():
+            try:
+                analytics_metadata = get_trailing_string_find(line.strip(), prefix)
+                # snyk version 1.1297.3 returns invalid json ending with branch=***
+                if analytics_metadata.endswith("***"):
+                    invalid_metadata = True
+                    analytics_metadata = analytics_metadata + '"}}}}}'
+                metadata_json = json.loads(analytics_metadata)
+                # print(f"json: {metadata_json}")
+            except json.JSONDecodeError as e:
+                print(f"Error at decoding analytics metadata line: {analytics_metadata} at position: {e.pos}")
+            # exit the stdin filtering
+            break
+        elif duration_ms_prefix in line.strip():
+            # handle potential Snyk CLI 1.1297.3 invalid metadata analytics json with durationMs property matching
+            raw_duration_ms = get_trailing_string_find(line.strip(), duration_ms_prefix)
+            # remove the trailing comma
+            duration_ms = int(raw_duration_ms[:-1])
+
+    # remove following if-check once Snyk CLI returns valid metadata analytics JSON
+    if invalid_metadata:
+        # insert the duration_ms property into the metadata json
+        runtime_perf_duration = {"runtime": {"performance": {"duration_ms": duration_ms}}}
+        metadata_json["data"]["attributes"].update(runtime_perf_duration)
+
+    return metadata_json
+
+
+def eval_manifests(complete, partial, manifest_files) -> dict:
+    """
+    Evaluates measurement of manifests by finding all elements missing from 'complete' that are not in 'partial',
+    assuming 'partial' is a strict subsequence of 'complete' i.e. manifests skipped,
+    relative order of scanned manifests is preserved.
+    :param complete: Complete list of package managers
+    :param partial: Partial scanned list of package managers
+    :param manifest_files: Manifest target files (path)
+    :return: Dictionary of measurements of scanned and skipped manifests
+    """
+    scanned_manifests = []
+    skipped_manifests = []
+    partial_idx = 0
+
+    for complete_idx, complete_item in enumerate(complete):
+        if partial_idx < len(partial) and complete_item == partial[partial_idx]:
+            # This item matches in sequence, so it is scanned
+            scanned_manifests.append(manifest_files[complete_idx])
+            partial_idx += 1
+        else:
+            # This item from the complete list was skipped in the partial list
+            skipped_manifests.append(manifest_files[complete_idx])
+
+    error_indicator = True if skipped_manifests else False
+    error_message = f"{len(skipped_manifests)}/{len(complete)} manifests encountered error at dependency resolution"
+    manifest_measurements = {
+        "scanned_manifests": scanned_manifests,
+        "skipped_manifests": skipped_manifests,
+        "error_indicator": error_indicator,
+        "error_message": error_message
+    }
+    return manifest_measurements
+
+
+def eval_manifests_metadata(metadata_json: dict):
+    """
+    Evaluates scan manifests metadata.
+    :param: metadata_json (Dict)
+    :return: manifest_measurements (Dict)
+    """
+    # check whether it is complete or partial manifest scan
+    all_projects_package_managers = metadata_json['legacycli::metadata__allProjects__packageManagers']
+    package_managers = all_projects_package_managers.strip("[]").split()
+    all_projects_target_files = metadata_json['legacycli::metadata__allProjects__targetFiles']
+    target_files = all_projects_target_files.strip("[]").split()
+
+    # compute measurements of cli processed manifests files (paths)
+    metadata_package_manager = metadata_json['legacycli::metadata__packageManager']
+    scanned_package_managers = metadata_package_manager.strip("[]").split()
+    manifest_measurements = eval_manifests(package_managers, scanned_package_managers, target_files)
+    # print(manifest_measurements)
+    return manifest_measurements
+
+
+def eval_scan_status(exit_code: int, error_at_manifest_scan: bool) -> str:
+    """
+    Evaluates scan status based on the snyk cli exit code with skipped manifests indicator
+    :param exit_code: cli exit code
+    :param error_at_manifest_scan: whether error at manifests scan (bool)
+    :return: status
+    """
+    if exit_code == 0 or exit_code == 1:
+        if error_at_manifest_scan:
+            status = "failure"
+        else:
+            status = "success"
+    else:
+        status = "failure"
+
+    return status
+
+
+def eval_cli_metadata(metadata_json) -> dict:
+    """
+    Evaluates raw cli analytics metadata json and formats required info into a dictionary
+    :param metadata_json:
+    :return: scan_results (Dict)
+    """
+    scan_results = {}
+    error_indicator = False
+
+    try:
+        extension_obj = metadata_json['data']['attributes']['interaction']['extension']
+        exitcode = extension_obj['exitcode']
+        # refer to https://docs.snyk.io/snyk-cli/commands/test#exit-codes
+        if exitcode < 2:
+            # scan is completed, check the manifests metadata of scan
+            manifest_measurements = eval_manifests_metadata(extension_obj)
+            error_indicator = manifest_measurements['error_indicator']
+            scan_results.update(manifest_measurements)
+        elif exitcode == 2:
+            # scan returns error during scan
+            error_message = "error at snyk test cli execution, examine the debug logs"
+            scan_results["error_message"] = error_message
+        elif exitcode == 3:
+            error_message = "failure due to no supported manifests detected"
+            scan_results["error_message"] = error_message
+
+        # evaluates scan status success if all manifests successfully scanned, failure otherwise
+        scan_status = eval_scan_status(exitcode, error_indicator)
+        # Temporarily disable duration metric on snyk 1.1297.3 broken JSON
+        scan_duration_ms = 0
+        if "runtime" in metadata_json['data']['attributes']:
+            scan_duration_ms = metadata_json['data']['attributes']['runtime']['performance']['duration_ms']
+        # enrich the scan results data
+        scan_results["status"] = scan_status
+        scan_results["duration_sec"] = scan_duration_ms / 1000
+        return scan_results
+    except KeyError as e:
+        print(f"Error at retrieving key from cli metadata json: {e}")
+        print(f"Ensure snyk cli cmd includes --all-projects --debug")
+        return {}
+
+
+if __name__ == "__main__":
+    analytics_prefix = "analytics.report:2 - [0] Data: "
+    metadata = extract_metadata_by_prefix(analytics_prefix)
+    if metadata:
+        metadata_summary = eval_cli_metadata(metadata)
+        # prints a pretty summary json
+        ms_json = json.dumps(metadata_summary, indent=2)
+        print(ms_json)
+    else:
+        print(f"Error at retrieving cli scan metadata line")
+        sys.exit(1)
